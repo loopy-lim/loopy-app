79d2f92eb5a6c4ed222ff9de08c29f311851f501
loopy-lim <loopy@jnu.ac.kr>
2025-01-14 22:12:23 +0900

    feat: add gluestack-ui

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,19 @@
 module.exports = function (api) {
   api.cache(true);
+  api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
+      "nativewind/babel"
     ],
-    plugins: ["expo-router/babel"],
+    plugins: ["expo-router/babel", ["module-resolver", {
+      root: ["./"],
+
+      alias: {
+        "@": "./",
+        "tailwind.config": "./tailwind.config.js"
+      }
+    }]],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,2 +1,6 @@
+const {
+  withNativeWind: withNativeWind
+} = require("nativewind/metro");
+
 require("ts-node/register");
 module.exports = require("./metro.config.ts");
