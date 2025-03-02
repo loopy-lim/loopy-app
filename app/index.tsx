fae9fcaf12d2bdc48cf432e5530286c02d70b65e
loopy-lim <loopy@jnu.ac.kr>
2025-03-02 22:25:44 +0900

    feat: add error boundary

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,17 @@
 import Logo from '@/components/common/Logo';
 import { TodoPreview } from '@/components/todo/Preview';
-import { Stack } from 'expo-router';
-import { SafeAreaView, View } from 'react-native';
+import { ErrorBoundaryProps, Stack } from 'expo-router';
+import { SafeAreaView, Text, View } from 'react-native';
+
+export function ErrorBoundary({ error, retry }: ErrorBoundaryProps) {
+  return (
+    <View style={{ flex: 1, backgroundColor: "red" }}>
+      <Text>{error.message}</Text>
+      <Text onPress={retry}>Try Again?</Text>
+    </View>
+  );
+}
+
 
 export default function HomePage() {
   return (
